299675702f540e4b07d576dba8ba97825b2eb43b
xXDuckXx <dacs2010@gmail.com>
2019-05-18 22:00:14 -0700

    fixed file path for static files and connect them index.html

---
+++ decks/static/js/app.js
@@ -0,0 +1,5 @@
+// - add eventlistener
+// - make an ajax call
+// - console log that json file
+
+console.log("connected")
--- mtg_deck_maker/settings.py
+++ mtg_deck_maker/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mtg_deck_maker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
